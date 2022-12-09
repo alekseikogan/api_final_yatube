fce2064c2fa3e7863c57a150ba622a4f4896c1c9
Алексей Коган <alekseikogan@yandex.ru>
2022-12-09 21:59:51 +0300

    1-я проверка

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -47,7 +47,7 @@ class Comment(models.Model):
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True)
-      
+
     def __str__(self):
         return self.text
 
