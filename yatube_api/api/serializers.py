b4f9ba77279f5d93f251e5788aa30975f273e6d3
Алексей Коган <alekseikogan@yandex.ru>
2022-12-10 21:21:01 +0300

    финальная

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,6 @@
-import base64
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
-from django.core.files.base import ContentFile
 from django.contrib.auth.models import User
 
 from posts.models import Comment, Post, Group, Follow
