9f8cb28ed91bebf9fa18584e39715fdc3ffa380f
Алексей Коган <alekseikogan@yandex.ru>
2022-12-10 20:58:28 +0300

    финальная

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -8,16 +8,6 @@ from django.contrib.auth.models import User
 from posts.models import Comment, Post, Group, Follow
 
 
-class Base64ImageField(serializers.ImageField):
-    def to_internal_value(self, data):
-        if isinstance(data, str) and data.startswith('data:image'):
-            format, imgstr = data.split(';base64,')
-            ext = format.split('/')[-1]
-            data = ContentFile(base64.b64decode(imgstr), name='temp.' + ext)
-
-        return super().to_internal_value(data)
-
-
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
