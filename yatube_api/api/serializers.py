090b179f90cb5f4068799e60cfd78fb1f02f463f
Алексей Коган <alekseikogan@yandex.ru>
2022-12-10 10:32:03 +0300

    после 1-й проверки

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,5 +1,4 @@
 import base64
-from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 from rest_framework.validators import UniqueTogetherValidator
 from django.core.files.base import ContentFile
